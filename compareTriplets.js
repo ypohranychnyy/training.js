b39e5ea253df4f18433d552014e992bf5340c1eb
Yuriy <the.whu@gmail.com>
2016-08-02 21:17:08 +0300

    incorrect values compaired

--- compareTriplets.js
+++ compareTriplets.js
@@ -1,19 +1,28 @@
-function main() {
-    var a0_temp = readLine().split(' ');
-    var b0_temp = readLine().split(' ');
-    
-    var a = 0;
-    var b = 0;
-    
-  for (i = 0; i < a0_temp.length-1; i++) {
+function compareArrayNymbers(stringOne, stringTwo) {
+  // console.log(stringOne);
+  var a0_temp = stringOne.split(' ');
+  // console.log(a0_temp)
+  var b0_temp = stringTwo.split(' ');
+
+  var a = 0;
+  var b = 0;
+
+  for ( var i = 0; i < a0_temp.length; i++) {
     if (a0_temp[i] > b0_temp[i]) {
       a++;
-    } else if (a === b) {
-      continue;
+    } else if (a0_temp[i] === b0_temp[i]) {
+      
     } else {
       b++;
     }
 
-  } console.log(a + ' ' + b);
+  } //console.log(`${a} ${b}`);
+  return (`${a} ${b}`);
 
 }
+
+console.log(compareArrayNymbers('1 2 3', '3 2 1') === '1 1'); // true
+console.log(compareArrayNymbers('1 2 3 6 5 7', '3 2 1 ') ); // what should be the result
+console.log(compareArrayNymbers('1 2 3', '3 2 1 4') === '1 1');
+console.log(compareArrayNymbers('', ''));
+console.log(compareArrayNymbers('1,2,3', '1,2,3'));
